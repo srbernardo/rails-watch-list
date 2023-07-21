2f7a2051b5e1480e63b6316267e5495a6980c02f
bernardo brotelli <b.brotelle@gmail.com>
2023-07-20 21:11:06 -0300

    Watch list finish

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:id])
+    @bookmark.list = @list
+    # @bookmark.list =
+
+    if @bookmark.save
+      redirect_to lists_path
+    else
+      render :new,  status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
+
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new"
+  post  "lists", to: "lists#create"
+  get "lists/:id", to: "lists#show", as: "list"
+
+  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: "list_bookmarks"
+  post "lists/:id/bookmarks/new", to: "bookmarks#create", as: 'new_list_bookmark'
+  delete "bookmarks/:id", to: "bookmarks#delete"
+
 end
