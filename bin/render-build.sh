e8f0d11db23345a702ed162411fa57bcaf25b4ba
bernardo-brotelli <b.brotelle@gmail.com>
2024-01-12 09:57:41 -0300

    test deploy 06

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:reset
-bundle exec rake generate devise:install
+bundle exec rails generate devise:install
 bundle exec rake db:migrate
